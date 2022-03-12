448c731ba585673ecf1f3ca41f2880b61651c0cc
akerkemyrzakhan <47209628+akerkemyrzakhan@users.noreply.github.com>
2022-03-13 00:56:27 +0600

    lab4

---
+++ angular-ypsudv-yhmmsq/src/app/products.ts
@@ -0,0 +1,87 @@
+export interface Product
+{
+  name:string,
+  rating:number,
+  description:string,
+  image:string,
+  link:string
+}
+
+export const products = [
+  {
+    name: 'Sceptre 24',
+    rating: 5,
+    description: 'Professional Thin 75Hz 1080p LED Monitor 2x HDMI VGA Build-in Speakers, Machine Black (E248W-19203R Series)',
+    image: 'https://m.media-amazon.com/images/I/71rXSVqET9L._AC_SL1257_.jpg',
+    link: 'https://www.amazon.com/Sceptre-E248W-19203R-Monitor-Speakers-Metallic/dp/B0773ZY26F/ref=lp_16225007011_1_3?th=1'
+  },
+  {
+    name: 'External Hard Drive',
+    rating: 4.9,
+    description: 'Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PlayStation, & Xbox - 1-Year Rescue Service (STGX2000400)',
+    image: 'https://m.media-amazon.com/images/I/81tjLksKixL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Seagate-Portable-External-Hard-Drive/dp/B07CRG94G3/ref=lp_16225007011_1_2?th=1'
+  },
+  {
+    name: 'Apple Pencil (2nd Generation)',
+    rating: 5.0,
+    description: 'Fake Compatible with iPad mini (6th generation), iPad Air (4th generation), iPad Pro 12.9-inch (3rd, 4th, and 5th generations), iPad Pro 11-inch (1st, 2nd, and 3rd generations) Camera Outdoor Waterproof Home Camera Solar Power Simulation Camera With LED light Security Surveillance',
+    image: 'https://m.media-amazon.com/images/I/41S3MKU9TjL._AC_SL1024_.jpg',
+    link: 'https://www.amazon.com/Apple-MU8F2AM-A-Pencil-Generation/dp/B07K1WWBJK/ref=lp_16225007011_1_1'
+  },
+  {
+    name: 'Wireless Keyboard and Mouse Combo',
+    rating: 4.6,
+    description: 'Logitech MK270 Wireless Keyboard and Mouse Combo for Windows, 2.4 GHz Wireless, Compact Mouse, 8 Multimedia and Shortcut Keys, 2-Year Battery Life, for PC, Laptop',
+    image: 'https://m.media-amazon.com/images/I/61pUul1oDlL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Logitech-MK270-Wireless-Keyboard-Mouse/dp/B079JLY5M5/ref=lp_16225007011_1_4'
+  },
+  {
+    name: 'Wireless Color Printer',
+    rating: 4.8,
+    description: 'HP DeskJet 4155e All-in-One Wireless Color Printer, with bonus 6 months free Instant Ink with HP+ (26Q90A)',
+    image: 'https://m.media-amazon.com/images/I/612G5AUOZWL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/HP-DeskJet-4155e-Wireless-Printer/dp/B08XYRDSL7/ref=lp_16225007011_1_6'
+  },
+  {
+    name: 'iPhone Charger',
+    rating: 4.6,
+    description: 'iPhone Charger, TAKAGI Lightning Cable 3PACK 6FT Nylon Braided USB Charging Cable High Speed Data Sync Transfer Cord Compatible with iPhone 13/12/11 Pro Max/XS MAX/XR/XS/X/8/7/Plus/6S/6/SE/5S/iPad',
+    image: 'https://m.media-amazon.com/images/I/71m-EsCA2aL._SL1500_.jpg',
+    link: 'https://www.amazon.com/TAKAGI-Charging-Connector-Transfer-Compatible/dp/B07PHSF8DP/ref=lp_16225007011_1_8'
+  },
+  {
+    name: 'Logitech C920x HD Pro Webcam',
+    rating: 4.9,
+    description: 'Full HD 1080p/30fps Video Calling, Clear Stereo Audio, HD Light Correction, Works with Skype, Zoom, FaceTime, Hangouts, PC/Mac/Laptop/Macbook/Tablet - Black',
+    image: 'https://m.media-amazon.com/images/I/71iNwni9TsL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Logitech-C920x-Pro-HD-Webcam/dp/B085TFF7M1/ref=lp_16225007011_1_12?th=1'
+  },
+  {
+    name: 'SAMSUNG 980 SSD',
+    rating: 4.9,
+    description: '1TB M.2 NVMe Interface Internal Solid State Drive with V-NAND Technology for Gaming, Heavy Graphics, Full Power Mode, MZ-V8V1T0B/AMa',
+    image: 'https://m.media-amazon.com/images/I/714VYiAYssL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/SAMSUNG-MZ-V8V1T0B-AM-980-SSD/dp/B08V83JZH4/ref=sr_1_23?qid=1647105543&s=computers-intl-ship&sr=1-23&th=1'
+  },
+  {
+    name: 'Dell USB 3.0',
+    rating: 4.6,
+    description: 'Ultra HD/4K Triple Display Docking Station (D3100), Black',
+    image: 'https://m.media-amazon.com/images/I/61qALmGqKGL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Dell-Display-Docking-Station-D3100/dp/B00O0M46KO/ref=sr_1_26?qid=1647105557&s=computers-intl-ship&sr=1-26'
+  },
+  {
+    name: 'Lenovo IdeaPad 3',
+    rating: 4.6,
+    description: 'Chromebook Laptop, 11.6" HD Display, Intel Celeron N4020, 4GB RAM, 64GB Storage, Intel UHD Graphics 600, Chrome OS',
+    image: 'https://m.media-amazon.com/images/I/71XIwK5G75L._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Lenovo-Chromebook-Processor-Graphics-82BA0003US/dp/B087YW8FQB/ref=sr_1_29?qid=1647105557&s=computers-intl-ship&sr=1-29'
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
